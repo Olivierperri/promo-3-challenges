e521417461fc356d3abc4d53f3682ffa1a38cfc5
Olivierperri <olivierperri73@gmail.com>
2014-07-08 15:36:54 +0200

    exercice 5 solved

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,20 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+
+if your_message == "I am going to work right now SIR !"
+  return ""
+elsif your_message.include? "?"
+  return "Silly question, get dressed and go to work !"
+else  "I don't care son, get dressed and go to work !"
+end
+
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if your_message include? "mcdo"
+    return "It's not good son"
 end
 
+end
+
+
+  # TODO: return coach answer to your_message, with additional custom rules of yours !
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,7 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+puts "Hello Coach ?"
+answer = gets.chomp
+
+puts coach_answer(answer)
