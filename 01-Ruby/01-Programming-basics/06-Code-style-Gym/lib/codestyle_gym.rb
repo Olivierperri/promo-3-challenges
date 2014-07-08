abd59a37f24785028f1b973962de8ccd6a519c41
Olivierperri <olivierperri73@gmail.com>
2014-07-08 18:03:38 +0200

    Exercice 6 solved

--- 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
+++ 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
@@ -1,53 +1,57 @@
-class Player_moving
-  Grid_POSITIONS = (1..100).to_a
+# define class PlayerMoving
+class PlayerMoving
+  grid_positions = (1..100).to_a
 
-def initialize (position)
-  @position = position
-  @iterations = 0
-  unless Grid_POSITIONS.include? @position
-    raise "Initial position must be between 1 and 100"
+  def initialize(position)
+    @position   = position
+    @iterations = 0
+
+    unless grid_positions.include?(@position)
+      fail "Initial position must be between 1 and 100"
+    end
   end
-end
 
-def move(distanceInMeters)
-@position += distanceInMeters
-end
+  def move(distance_in_meters)
+    @position += distance_in_meters
+  end
 
-def play
-  if(self.status_Player==:play)
-    if rand(1..100) < 50
-      move(rand(1..10))
+  def play
+    if status_player == :play
+      if rand(1..100) < 50
+        move(rand(1..10))
+      else
+        move(-rand(1..10))
+      end
+    end
+  end
+
+  def cheat
+    @position = 101
+  end
+
+  def status_player
+    if @position > 100
+      :win
+    elsif @position < 0
+      :loose
     else
-      move(-rand(1..10))
+      :play
     end
+  end
+
+  def won?
+    status_player == :win
   end
 end
 
-def cheat
-  @position = 101
-end
-
-def status_Player
-    if(@position>100)  :win
-    elsif(@position<0) :loose
-    else  :play
-    end
-end
-
-def has_won
- status_Player  == :win
-end
-
-end
-
-player = Player_moving.new(50)
+player = PlayerMoving.new(50)
 player.play
-puts player.status_Player == :play # true
+puts player.status_player == :play # true
 player.cheat
-puts player.has_won == true
+puts player.won? == true
 
 begin
-  Player_moving.new(110)
+  PlayerMoving.new(110)
   rescue RuntimeError => e
-  puts e.to_s == "Initial position must be between 1 and 100" # true
+    puts e.to_s == "Initial position must be between 1 and 100" # true
 end
