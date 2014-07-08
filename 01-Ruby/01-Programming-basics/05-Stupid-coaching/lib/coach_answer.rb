77b8805812b549a2ad92a8466fbfbef2f33060f2
Olivierperri <olivierperri73@gmail.com>
2014-07-08 18:02:31 +0200

    exercice 5 solved

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,17 +1,16 @@
 def coach_answer(your_message)
-
-if your_message == "I am going to work right now SIR !"
+  if your_message == "I am going to work right now SIR !"
   return ""
-elsif your_message.include? "?"
+  elsif your_message.include? "?"
   return "Silly question, get dressed and go to work !"
-else  "I don't care son, get dressed and go to work !"
+  else  "I don't care son, get dressed and go to work !"
 end
 
 end
 
 def coach_answer_enhanced(your_message)
   if your_message include? "mcdo"
-    return "It's not good son"
+  return "It's not good son"
 end
 
 end
