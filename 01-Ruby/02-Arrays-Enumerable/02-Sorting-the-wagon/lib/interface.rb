2698f3d6e6092779ac9fe42a603f506e7b9a0bd3
Olivierperri <olivierperri73@gmail.com>
2014-07-09 14:50:43 +0200

    sorting the wagon

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,4 +1,4 @@
-require_relative "wagon_sort"
+require_relative 'wagon_sort'
 
 students = []
 
@@ -8,7 +8,18 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
+  students << name if name != ""
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+
+list = wagon_sort(students)
+count = students.size
+
+display = wagon_display(list, count)
+
+puts "Congratulations! Your Wagon has #{count} students : "
+
+puts display
+
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,12 @@
 def wagon_sort(students)
-  # TODO: return a copy of students, sorted alphabetically
+  students.sort_by{|word| word.downcase}
+end
+
+def wagon_display(students, count)
+
+  part1 =  students[0..count-2].join(", ")
+  part2 =  students.last
+  sentence = "#{part1} and #{part2}"
+  return sentence
+
 end
