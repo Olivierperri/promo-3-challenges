137a11d2286e3d34bb5f6da30da3041cb3335e54
Olivierperri <olivierperri73@gmail.com>
2014-07-09 17:04:00 +0200

    Blackjack4life

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,15 @@
 def bank_score
+  rand(16..21)
   # TODO: Use Random to get a new random score
 end
 
 def pick_card
+  rand(1..11)
+
   # TODO: Use Random to get a new random card
 end
 
 def game_outcome(bank, score)
+  [bank, score]
   # TODO: Take the bank and the score and output an array containing the bank and then the score
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,17 +1,55 @@
 require_relative 'black_jack'
 
 def play_game
-  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+  #determine score joueur et bank
+  score_bank = bank_score
+  score_player = 0
+
+    while asking_for_card?(score_player) == true
+    score_player += pick_card
+    puts state_of_the_game(score_player, score_bank)
+    end
+
+    outcome = game_outcome(score_bank, bank_score)
+    puts build_message_for(outcome)
 end
 
+
+  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
 def state_of_the_game(score, bank)
+  "Your score is #{score}, bank is #{bank}!"
   # TODO: Returns custom message with player's score and bank's score
 end
 
 def asking_for_card?(score)
-  # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+  if score <= 21
+  puts "Card ? (type 'Y' or 'yes' for a new card)"
+  answer = gets.chomp
+    if answer == 'Y' || answer == 'yes'
+      true
+    else
+      false
+    end
+  else
+    false
+  end
 end
 
+  # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+
+
 def build_message_for(outcome)
-  #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+  if outcome[1] > 21
+    "You are over 21... you loose."
+  elsif outcome[1] == 21
+    "Black Jack!"
+  elsif
+    outcome[1] < outcome[0]
+    "Bank beats you! You loose."
+  else
+     "You beat the bank! You win."
+  end
 end
+
+#TODO: return specific message depending on the game outcome (= bank's score and user's score)
