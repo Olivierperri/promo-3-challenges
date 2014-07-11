277911f816af896b722ffd7aa1eb89e780a0cb3c
Olivierperri <olivierperri73@gmail.com>
2014-07-11 14:07:49 +0200

    symbols solved

--- 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
@@ -8,11 +8,11 @@
 # irb> "foo".object_id
 
 def are_identical_symbols_same_objects?
-  # TODO: true or false?
+  true
 end
 
 def are_identical_strings_same_objects?
-  # TODO: true or false?
+  false
 end
 
 
@@ -21,17 +21,24 @@ end
 # - http://www.ruby-doc.org/core-2.1.1/Symbol.html
 
 def convert_string_to_symbol(a_string)
-  # TODO: return the symbol version of "a_string"
+  a_string.to_sym
 end
 
 def convert_symbol_to_string(a_symbol)
-  # TODO: return the string version of "a_symbol"
+  a_symbol.to_s# TODO: return the string version of "a_symbol"
 end
 
 def me
+  me = {
+  :name => "Olivier",
+  :age => 24
+}
+return me
   # TODO: return a Hash representing yourself, with keys such as age and name
 end
 
 def fruits
+  fruit = ["orange","lemon","strawberry"]
+  return fruit
   # TODO: return an array of fruits
 end
