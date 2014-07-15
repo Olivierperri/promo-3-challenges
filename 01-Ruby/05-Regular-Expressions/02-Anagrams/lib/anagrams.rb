e23dc7989e69e12f401e08d31c4086e42e364f99
Olivierperri <olivierperri73@gmail.com>
2014-07-15 15:23:09 +0200

    anagrams solved

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -11,6 +11,20 @@ def anagrams?(a_string, another_string)
 end
 
 def anagrams_on_steroids?(a_string, another_string)
-  #Hash
-  #TODO: implement the improved method
+  if string_to_h(a_string) == string_to_h(another_string)
+    true
+  else
+    false
+  end
 end
+
+def string_to_h(a_string)
+  hash = {}
+  string_tab = a_string.downcase.scan(/\w/)
+
+  string_tab.each  do |x|
+  hash[x.to_sym] = string_tab.count(x)
+  end
+return hash
+end
+
