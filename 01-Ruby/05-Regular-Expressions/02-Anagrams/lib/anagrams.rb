31bf17346fce41bd7cb72f808f639a7a304f7861
Olivierperri <olivierperri73@gmail.com>
2014-07-15 12:11:14 +0200

    anagram part 1 solved

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,16 @@
 def anagrams?(a_string, another_string)
+  string1 = a_string.gsub(//)
+  string2 = another_string.gsub(//)
+
+  if string1.sort == string2.sort
+    return true
+  else
+    return false
+  end
   #TODO: implement the obvious method to test if two words are anagrams
 end
 
 def anagrams_on_steroids?(a_string, another_string)
+  #Hash
   #TODO: implement the improved method
 end
-  
