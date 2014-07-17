417eb4d82d5de225afb7c20a83f37702425dbe88
Olivierperri <olivierperri73@gmail.com>
2014-07-17 12:44:35 +0200

    exo solved

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,32 @@
 class Restaurant
-  
+
+  attr_reader :average_rating
+  attr_accessor :city, :name
   #TODO add relevant accessors if necessary
-  
   def initialize(city, name)
+    @city = city
+    @name = name
+    @rate = []
+    @average_rating = 0
     #TODO: implement constructor with relevant instance variables
   end
-  
-  #TODO: implement #filter_by_city and #rate methods
-  
+
+  def rate(grade)
+    @rate << grade
+    @average_rating = @rate.inject(:+) / @rate.size
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants_list = []
+
+    restaurants.each do |resto|
+      if resto.city == city
+        restaurants_list << resto
+      end
+    end
+
+    restaurants_list
+  end
+
 end
+  #TODO: implement #filter_by_city and #rate method
