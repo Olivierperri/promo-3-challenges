5a65f2f7bdd30a85c3492d6abb1a3006e777f256
Olivierperri <olivierperri73@gmail.com>
2014-07-19 19:09:06 +0200

    exercice solved

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
@@ -1,9 +1,31 @@
+require "csv"
 require_relative 'cookbook'
+require_relative 'ui'
+require_relative 'recipe'
 
 class Controller
-  @repository = cookbook.new
-  @display =  Ui.new
+
+  def initialize(cookbook)
+    @cookbook = cookbook
+    @ui = UI.new
   end
+
+  def list
+    recipe = @cookbook.list_all_recipes
+    @ui.display_list(recipe)
+  end
+
+  def create
+    choice = @ui.ask_to_add_a_recipe
+    @cookbook.add_recipe(choice)
+  end
+
+  def destroy
+    index = @ui.ask_to_remove_a_recipe
+    @cookbook.remove_recipe(index)
+  end
+
+ end
 
   #Here you should instantiate the Cookbook model with the file
 #   end
--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
@@ -1,60 +1,58 @@
 require 'csv'
+require_relative "recipe"
+require_relative "controller"
+require_relative "ui"
 
 class Cookbook
-  attr_reader :file, :recipes
+  attr_reader  :recipes
 
-  def initialize(file)
-    @file = file
+  def initialize(csv_file)
+    @file = csv_file
     @recipes = []
+    load_csv(@file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    #codebyPerri
   end
 
   def list_all_recipes
     @recipes = []
-    CSV.foreach(@file) do |row|
-      @recipes << row[0]
+    CSV.foreach(@file, col_sep: ',') do |row|
+      @recipes << [row[0],row[1]]
     end
     return @recipes
   end
 
-  def add_a_recipe(recipe)
-    @recipes << recipe
-    CSV.open(@file, 'w' ) do |csv|
-    @recipes.each do |recipe|
-    csv << [recipe]
-    end
+  def load_csv(file)
+    CSV.foreach(@file) do |row|
+      @recipes << Recipe.new(row[0], row[1])
     end
   end
 
 
-  def delete_a_recipe(recipe)
-    @recipes.delete(recipe)
-      CSV.open(@file, 'w' ) do |csv|
-        @recipes.each do |recipe|
+  def save_csv
+    CSV.open(@file, 'w',  col_sep: ',') do |csv|
+      @recipes.each do |recipe| # un csv attend toujours une injection de tableau
         csv << [recipe]
-        end
       end
+    end
   end
 
+  def add_recipe(recipe)
+    @recipes << [recipe.name,recipe.description]
+    save_csv
+  end
+
+  def remove_recipe(recipe_id)
+    @recipes.delete_at(recipe_id)
+    save_csv
+  end
+
+
+
+end
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
   # TODO: Implement a save method that will write the data into the CSV
   # And don't forget to use this save method when you have to modify something in your recipes array.
-end
-
-
-cook1 = Cookbook.new("recipes.csv")
-cook1.add_a_recipe("Paella")
-cook1.add_a_recipe("Tarte")
-cook1.add_a_recipe("Pizza")
-
-cook1.list_all_recipes
-cook1.add_a_recipe("tarte aux pommes")
-cook1.delete_a_recipe("Tarte")
-cook1.delete_a_recipe("Pizza")
-cook1.list_all_recipes
-cook1.add_a_recipe("tarte aux framboises")
-
-puts cook1.list_all_recipes
 
 
