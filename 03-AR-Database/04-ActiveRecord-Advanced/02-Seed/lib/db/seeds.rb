74b743591e37e96d51ce20284a2390afa8501f60
Olivierperri <olivierperri73@gmail.com>
2014-07-29 16:22:12 +0200

    test

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
@@ -6,7 +6,17 @@ require 'faker'
 require 'nokogiri'
 require 'open-uri'
 
-puts 'Seeding database...'
+puts "Seeding database for Y comboinator, based on #{https://news.ycombinator.com/}. Let's rock the party!"
+puts '-'*50
+
+puts 'Removing data...'
+User.delete_all
+Post.delete_all
+
+puts 'Generating user, and post...'
+require  'faker'
+
+
 
 # TODO: Your code goes here
 # 1. create fake users
